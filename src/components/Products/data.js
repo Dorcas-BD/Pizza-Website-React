d326a197cf59903f24fa540691a43427f900b28f
Dorcas-BD <52034693+Dorcas-BD@users.noreply.github.com>
2021-12-31 11:48:23 +0100

    commiy

---
+++ src/components/Products/data.js
@@ -0,0 +1,61 @@
+import product1 from '../../images/2.jpg'
+import product2 from '../../images/5.jpg'
+import product3 from '../../images/10.jpg'
+import sweet1 from '../../images/12.jpg'
+import sweet2 from '../../images/14.jpg'
+import sweet3 from '../../images/0.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    }
+]
+
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    },
+    {
+        img: sweet2,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    },
+    {
+        img: sweet3,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, Italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to cart'
+    }
+]
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,11 +1,35 @@
-import React from 'react' 
+import React from 'react'
+import {
+    ProductsContainer,
+    ProductWrapper,
+    ProductsHeading,
+    ProductTitle,
+    ProductCard,
+    ProductImg,
+    ProductInfo,
+    ProductDesc,
+    ProductPrice,
+    ProductButton,
+} from './ProductsElement';
 
-const Products = () => {
+const Products = ({heading, data}) => {
     return (
         <ProductsContainer>
-            <ProductsHeading>Heading</ProductsHeading>
+            <ProductsHeading>{heading}</ProductsHeading>
             <ProductWrapper>
-            
+                {data.map((product, index) => {
+                    return (
+                        <ProductCard key={index}>
+                            <ProductImg src={product.img} alt={product.alt} />
+                            <ProductInfo>
+                                <ProductTitle>{product.name}</ProductTitle>
+                                <ProductDesc>{product.desc}</ProductDesc>
+                                <ProductPrice>{product.price}</ProductPrice>
+                                <ProductButton>{product.button}</ProductButton>
+                            </ProductInfo>
+                        </ProductCard>
+                    )
+                })}
             </ProductWrapper>
         </ProductsContainer>
     )
